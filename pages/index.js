2dd9301c7ec436bc3cec429a3133423d28b01d32
Shubham Chitransh <shubhamchitransh8@gmail.com>
2020-09-17 10:00:09 +0530

    setup tailwind css with nextjs

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="py-20">
+        <h1 className="text-5xl text-center">NextJs With Tailwind</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
